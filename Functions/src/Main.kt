7be651f27ea1e805720b69e007571acfb53bf063
DalalBakhsh98 <69141269+DalalBakhsh98@users.noreply.github.com>
2025-02-12 17:44:09 +0300

    Updated Main.kt
    Corrected the var names

--- Functions/src/Main.kt
+++ Functions/src/Main.kt
@@ -1,22 +1,22 @@
 fun main() {
     greetTraveler()
     println(findOasis(10, 20))
-    println("Average Temperature:"+currentTemperature(25,40))
+    println("Average Temperature:"+ currentTemperature(25,40))
 }
 
 fun greetTraveler(){
     println("Welcome to the Desert, Traveler!")
 }
 
-fun findOasis(x:Int , y:Int): String {
+fun findOasis(x: Int , y: Int): String {
     return "Oasis found at coordinates ($x, $y)"
 }
 
-fun currentTemperature(morningTemperature:Int , afternoonTemperature:Int): Int {
-    var average:Int
-    average = (morningTemperature+afternoonTemperature)/2
+fun currentTemperature(morningTemperature: Int , afternoonTemperature: Int): Int {
+    var averageTemperature: Int
+    averageTemperature = (morningTemperature + afternoonTemperature)/2
 
-    return average
+    return averageTemperature
 
     //this function was a trick for me! Watch out!!
 
